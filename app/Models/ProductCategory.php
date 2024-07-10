2ecf32dea4e7493a17349083b44922cc259d21e2
mlemingcapoo <ducvu73@gmail.com>
2024-07-11 03:07:39 +0700

    Update: Fix model not found error

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    protected $table = 'product_categories';
+    public $incrementing = true;
+    protected $fillable = [
+        'product_id',
+        'category_id'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+}
