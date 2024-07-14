f5c4f6f052af23987b095464a813a31f1d2f442e
ntl1997 <luandajk4@gmail.com>
2024-07-14 16:01:18 +0700

    viet man contact

---
+++ app/Http/Controllers/Client/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+use App\Http\Controllers\Controller;
+use App\Service\client\ContactService;
+use Illuminate\Http\Request;
+class ContactController extends Controller
+{
+    private $contactService;
+
+    public function __construct(ContactService $contactService) {
+        $this->contactService = $contactService;
+    }
+    public function index(){
+        $contactUs = $this->contactService->getAll();
+        return view('client.contact.index',compact('contactUs'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Client\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('client.home');
+// Route::get('/', function () {
+//     // return view('client.home');
+// });
+Route::prefix('contact')->group(function (){
+    Route::get('/',[ContactController::class, 'index'])->name('contact.index');
 });
