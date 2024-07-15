d27f710913b997bc8cf86a63a8854dd6ee5979d5
ntl1997 <luandajk4@gmail.com>
2024-07-15 20:13:35 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\client\ContactController;
-use App\Http\Controllers\Client\CustomerRequestController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -9,5 +8,5 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::prefix('contact')->group(function () {
     Route::get('/', [ContactController::class, 'index'])->name('client.contact.index');
-    Route::post('/', [CustomerRequestController::class, 'store'])->name('customer.requests.store');
+    Route::post('/', [ContactController::class, 'store'])->name('customer.requests.store');
 });
