3d17c298affcb0c0c672fbf445ddfc10ba342f5f
letnp1024 <letnp1024@gmail.com>
2024-07-15 23:05:33 +0700

    Sửa ảnh phần instagram file layout

--- resources/views/client/layout.blade.php
+++ resources/views/client/layout.blade.php
@@ -115,7 +115,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/instagram/insta-1.jpg">
+                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/product/product-1.jpg">
                         <div class="instagram__text">
                             <i class="fa fa-instagram"></i>
                             <a href="#">@ ashion_shop</a>
@@ -123,7 +123,7 @@
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/instagram/insta-2.jpg">
+                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/product/product-2.jpg">
                         <div class="instagram__text">
                             <i class="fa fa-instagram"></i>
                             <a href="#">@ ashion_shop</a>
@@ -131,7 +131,7 @@
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/instagram/insta-3.jpg">
+                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/product/product-3.jpg">
                         <div class="instagram__text">
                             <i class="fa fa-instagram"></i>
                             <a href="#">@ ashion_shop</a>
@@ -139,7 +139,7 @@
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/instagram/insta-4.jpg">
+                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/product/product-4.jpg">
                         <div class="instagram__text">
                             <i class="fa fa-instagram"></i>
                             <a href="#">@ ashion_shop</a>
@@ -147,7 +147,7 @@
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/instagram/insta-5.jpg">
+                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/product/product-5.jpg">
                         <div class="instagram__text">
                             <i class="fa fa-instagram"></i>
                             <a href="#">@ ashion_shop</a>
@@ -155,7 +155,7 @@
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/instagram/insta-6.jpg">
+                    <div class="instagram__item set-bg" data-setbg="{{ url('') . '/'}}img/product/product-6.jpg">
                         <div class="instagram__text">
                             <i class="fa fa-instagram"></i>
                             <a href="#">@ ashion_shop</a>
