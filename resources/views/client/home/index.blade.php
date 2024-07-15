0211bfdb35c44e26afb26dcb2c524816c6eb5153
letnp1024 <letnp1024@gmail.com>
2024-07-15 21:28:01 +0700

    Dịch layout và home

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -1,7 +1,7 @@
 
 @extends('client.layout')
 @section('main')
-   <!-- Categories Section Begin -->
+   <!-- Phần Danh Mục Bắt Đầu -->
     <section class="categories">
         <div class="container-fluid">
             <div class="row">
@@ -10,8 +10,8 @@
                     data-setbg="{{ asset($productAmount[0]->img) }}">
                     <div class="categories__text">
                         <h1 style="font-family: 'Times New Roman', Times, serif, fantasy"> Thời trang {{ $productAmount[0]->name }}</h1>
-                        <p>{{ $productAmount[0]->product_amount }} items</p>
-                        <a href="#">Shop now</a>
+                        <p>{{ $productAmount[0]->product_amount }} sản phẩm</p>
+                        <a href="#">Mua ngay</a>
                     </div>
                 </div>
             </div>
@@ -23,8 +23,8 @@
                         <div class="categories__item set-bg" data-setbg="{{ asset($category->img) }}">
                             <div class="categories__text">
                                 <h4 style="font-family: 'Times New Roman', Times, serif, fantasy;">Thời trang {{ $category->name }}</h4>
-                                <p>{{ $category->product_amount }} items</p>
-                                <a href="#">Shop now</a>
+                                <p>{{ $category->product_amount }} sản phẩm</p>
+                                <a href="#">Mua ngay</a>
                             </div>
                         </div>
                     </div>
@@ -34,25 +34,25 @@
         </div>
     </div>
 </section>
-<!-- Categories Section End -->
+<!-- Phần Danh Mục Kết Thúc -->
 
-<!-- Product Section Begin -->
+<!-- Phần Sản Phẩm Bắt Đầu -->
 <section class="product spad">
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-md-4">
                 <div class="section-title">
-                    <h4>New product</h4>
+                    <h4>Sản phẩm mới</h4>
                 </div>
             </div>
             <div class="col-lg-8 col-md-8">
                 <ul class="filter__controls">
-                    <li class="active" data-filter="*">All</li>
-                    <li data-filter=".Nữ">Women’s</li>
-                    <li data-filter=".Nam">Men’s</li>
-                    <li data-filter=".Trẻ em">Kid’s</li>
+                    <li class="active" data-filter="*">Tất cả</li>
+                    <li data-filter=".Nữ">Nữ</li>
+                    <li data-filter=".Nam">Nam</li>
+                    <li data-filter=".Trẻ em">Trẻ em</li>
                     <li data-filter=".Unisex">Unisex</li>
-                    {{-- <li data-filter=".cosmetic">Cosmetics</li> --}}
+                    {{-- <li data-filter=".cosmetic">Mỹ phẩm</li> --}}
                 </ul>
             </div>
         </div>
@@ -61,7 +61,7 @@
             <div class="col-lg-3 col-md-4 col-sm-6 mix @foreach($product->categories as $category){{ $category->name }} @endforeach ">       
                 <div class="product__item">
                     <div class="product__item__pic set-bg" data-setbg="{{ asset($product->img) }}">
-                        <div class="label new">New</div>
+                        <div class="label new">Mới</div>
                         <ul class="product__hover">
                             <li><a href="{{ asset($product->img) }}" class="image-popup"><span class="arrow_expand"></span></a></li>
                             <li><a href="#"><span class="icon_heart_alt"></span></a></li>
@@ -77,7 +77,7 @@
                             <i class="fa fa-star"></i>
                             <i class="fa fa-star"></i>
                         </div>
-                        <div class="product__price">{{ number_format((int) $product->price, 0) }}</div>
+                        <div class="product__price">{{ number_format((int) $product->price, 0) }} VND</div>
                     </div>
                 </div>
             </div>
@@ -85,9 +85,9 @@
         </div>
     </div>
 </section>
-<!-- Product Section End -->
+<!-- Phần Sản Phẩm Kết Thúc -->
 
-<!-- Banner Section Begin -->
+<!-- Phần Banner Bắt Đầu -->
 <section class="banner set-bg" data-setbg="{{ asset($collectionBanner[0]->image) }}">
     <div class="container">
         <div class="row">
@@ -98,7 +98,7 @@
                         <div class="banner__text">
                             <span>{{ $banner->title }}</span>
                             <h1>{{ $banner->subtitle  }}</h1>
-                            <a href="{{ $banner->link }}">Shop now</a>
+                            <a href="{{ $banner->link }}">Mua ngay</a>
                         </div>
                     </div>
                     @endforeach
@@ -107,15 +107,15 @@
         </div>
     </div>
 </section>
-<!-- Banner Section End -->
+<!-- Phần Banner Kết Thúc -->
 
-<!-- Trend Section Begin -->
+<!-- Phần Xu Hướng Bắt Đầu -->
 <section class="trend spad">
     <div class="container">
         <div class="row">
             @php
                 $count = 0;
-                $sections = ['Hot Trend', 'Best Seller'];
+                $sections = ['Xu Hướng Hot', 'Bán Chạy Nhất'];
             @endphp
             @foreach($trendProductInfo as $product)
                 @if($count % 3 == 0)
@@ -126,7 +126,7 @@
                     <div class="col-lg-4 col-md-4 col-sm-6">
                         <div class="trend__content">
                             <div class="section-title">
-                                <h4>{{ $sections[$count / 3] ?? 'Other Products' }}</h4>
+                                <h4>{{ $sections[$count / 3] ?? 'Sản Phẩm Khác' }}</h4>
                             </div>
                 @endif
                 <div class="trend__item">
@@ -156,7 +156,7 @@
           <div class="col-lg-4 col-md-4 col-sm-6">
                 <div class="trend__content">
                     <div class="section-title">
-                        <h4>Favorite</h4>
+                        <h4>Ưa Thích</h4>
                     </div>
                     @foreach($favoriteProductInfo as $product)
                     <div class="trend__item">
@@ -181,9 +181,9 @@
         </div>
     </div>
 </section>
-<!-- Trend Section End -->
+<!-- Phần Xu Hướng Kết Thúc -->
 
-<!-- Discount Section Begin -->
+<!-- Phần Giảm Giá Bắt Đầu -->
 <section class="discount">
     <div class="container">
         <div class="row">
@@ -200,71 +200,70 @@
             <div class="col-lg-6 p-0">
                 <div class="discount__text">
                     <div class="discount__text__title">
-                        <span>Discount</span>
+                        <span>Giảm Giá</span>
                         <h2>{{ $latestPromotion->description }}</h2>
-                        <h5><span>Sale</span> {{ $latestPromotion->discount_percentage }}%</h5>
+                        <h5><span>Giảm</span> {{ $latestPromotion->discount_percentage }}%</h5>
                     </div>
                     <div class="discount__countdown" id="countdown-time">
                         <div class="countdown__item">
                             <span id="days">{{ $latestPromotion->daysDiff }}</span>
-                            <p>Days</p>
+                            <p>Ngày</p>
                         </div>
                         <div class="countdown__item">
                             <span id="hours">{{ $latestPromotion->hoursDiff }}</span>
-                            <p>Hours</p>
+                            <p>Giờ</p>
                         </div>
                         <div class="countdown__item">
                             <span id="minutes">{{ $latestPromotion->minsDiff }}</span>
-                            <p>Minutes</p>
+                            <p>Phút</p>
                         </div>
                         <div class="countdown__item">
                             <span id="seconds">{{ $latestPromotion->secsDiff }}</span>
-                            <p>Seconds</p>
+                            <p>Giây</p>
                         </div>
                     </div>
-                                        <a href="#">Shop now</a>
+                    <a href="#">Mua ngay</a>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!-- Discount Section End -->
+<!-- Phần Giảm Giá Kết Thúc -->
 
-<!-- Services Section Begin -->
+<!-- Phần Dịch Vụ Bắt Đầu -->
 <section class="services spad">
     <div class="container">
         <div class="row">
             <div class="col-lg-3 col-md-4 col-sm-6">
                 <div class="services__item">
                     <i class="fa fa-car"></i>
-                    <h6>Free Shipping</h6>
-                    <p>For all oder over $99</p>
+                    <h6>Miễn Phí Vận Chuyển</h6>
+                    <p>Cho tất cả đơn hàng trên $99</p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6">
                 <div class="services__item">
                     <i class="fa fa-money"></i>
-                    <h6>Money Back Guarantee</h6>
-                    <p>If good have Problems</p>
+                    <h6>Bảo Đảm Hoàn Tiền</h6>
+                    <p>Nếu hàng có vấn đề</p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6">
                 <div class="services__item">
                     <i class="fa fa-support"></i>
-                    <h6>Online Support 24/7</h6>
-                    <p>Dedicated support</p>
+                    <h6>Hỗ Trợ Trực Tuyến 24/7</h6>
+                    <p>Hỗ trợ tận tâm</p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6">
                 <div class="services__item">
                     <i class="fa fa-headphones"></i>
-                    <h6>Payment Secure</h6>
-                    <p>100% secure payment</p>
+                    <h6>Thanh Toán An Toàn</h6>
+                    <p>Thanh toán 100% an toàn</p>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!-- Services Section End -->
+<!-- Phần Dịch Vụ Kết Thúc -->
 @endsection
-
--- resources/views/client/layout.blade.php
+++ resources/views/client/layout.blade.php
@@ -1,18 +1,18 @@
+
 <!DOCTYPE html>
-<html lang="zxx">
+<html lang="vi">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Ashion Template">
-    <meta name="keywords" content="Ashion, unica, creative, html">
+    <meta name="description" content="Mẫu Ashion">
+    <meta name="keywords" content="Ashion, unica, sáng tạo, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ashion | Template</title>
+    <title>Ashion | Mẫu</title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <!-- Css Styles -->
     <link rel="stylesheet" href="{{ url('') . '/'}}css/bootstrap.min.css" type="text/css">
@@ -26,36 +26,36 @@
 </head>
 
 <body>
-    <!-- Page Preloder -->
+    <!-- Trình tải trang -->
     <div id="preloder">
         <div class="loader"></div>
     </div>
 
-    <!-- Offcanvas Menu Begin -->
+    <!-- Bắt đầu Menu Offcanvas -->
     <div class="offcanvas-menu-overlay"></div>
     <div class="offcanvas-menu-wrapper">
         <div class="offcanvas__close">+</div>
         <ul class="offcanvas__widget">
             <li><span class="icon_search search-switch"></span></li>
             <li><a href="#"><span class="icon_heart_alt"></span>
-                    <div class="tip">2</div>
-                </a></li>
+                <div class="tip">2</div>
+            </a></li>
             <li><a href="#"><span class="icon_bag_alt"></span>
-                    <div class="tip">2</div>
-                </a></li>
+                <div class="tip">2</div>
+            </a></li>
         </ul>
         <div class="offcanvas__logo">
             <a href="./index.html"><img src="{{ url('') . '/'}}img/logo.png" alt=""></a>
         </div>
         <div id="mobile-menu-wrap"></div>
         <div class="offcanvas__auth">
-            <a href="#">Login</a>
-            <a href="#">Register</a>
+            <a href="#">Đăng nhập</a>
+            <a href="#">Đăng ký</a>
         </div>
     </div>
-    <!-- Offcanvas Menu End -->
+    <!-- Kết thúc Menu Offcanvas -->
 
-    <!-- Header Section Begin -->
+    <!-- Bắt đầu Phần Header -->
     <header class="header">
         <div class="container-fluid">
             <div class="row">
@@ -67,37 +67,37 @@
                 <div class="col-xl-6 col-lg-7">
                     <nav class="header__menu">
                         <ul>
-                            <li class="active"><a href="./index.html">Home</a></li>
-                            <li><a href="#">Women’s</a></li>
-                            <li><a href="#">Men’s</a></li>
-                            <li><a href="./shop.html">Shop</a></li>
-                            <li><a href="#">Pages</a>
+                            <li class="active"><a href="./index.html">Trang chủ</a></li>
+                            <li><a href="#">Nữ</a></li>
+                            <li><a href="#">Nam</a></li>
+                            <li><a href="./shop.html">Cửa hàng</a></li>
+                            <li><a href="#">Trang</a>
                                 <ul class="dropdown">
-                                    <li><a href="./product-details.html">Product Details</a></li>
-                                    <li><a href="./shop-cart.html">Shop Cart</a></li>
-                                    <li><a href="./checkout.html">Checkout</a></li>
-                                    <li><a href="./blog-details.html">Blog Details</a></li>
+                                    <li><a href="./product-details.html">Chi tiết sản phẩm</a></li>
+                                    <li><a href="./shop-cart.html">Giỏ hàng</a></li>
+                                    <li><a href="./checkout.html">Thanh toán</a></li>
+                                    <li><a href="./blog-details.html">Chi tiết Blog</a></li>
                                 </ul>
                             </li>
                             <li><a href="./blog.html">Blog</a></li>
-                            <li><a href="./contact.html">Contact</a></li>
+                            <li><a href="./contact.html">Liên hệ</a></li>
                         </ul>
                     </nav>
                 </div>
                 <div class="col-lg-3">
                     <div class="header__right">
                         <div class="header__right__auth">
-                            <a href="#">Login</a>
-                            <a href="#">Register</a>
+                            <a href="#">Đăng nhập</a>
+                            <a href="#">Đăng ký</a>
                         </div>
                         <ul class="header__right__widget">
                             <li><span class="icon_search search-switch"></span></li>
                             <li><a href="#"><span class="icon_heart_alt"></span>
-                                    <div class="tip">2</div>
-                                </a></li>
+                                <div class="tip">2</div>
+                            </a></li>
                             <li><a href="#"><span class="icon_bag_alt"></span>
-                                    <div class="tip">2</div>
-                                </a></li>
+                                <div class="tip">2</div>
+                            </a></li>
                         </ul>
                     </div>
                 </div>
@@ -107,10 +107,10 @@
             </div>
         </div>
     </header>
-    <!-- Header Section End -->
+    <!-- Kết thúc Phần Header -->
     @yield('main')
 
-    <!-- Instagram Begin -->
+    <!-- Bắt đầu Instagram -->
     <div class="instagram">
         <div class="container-fluid">
             <div class="row">
@@ -165,9 +165,9 @@
             </div>
         </div>
     </div>
-    <!-- Instagram End -->
+    <!-- Kết thúc Instagram -->
 
-    <!-- Footer Section Begin -->
+    <!-- Bắt đầu Phần Footer -->
     <footer class="footer">
         <div class="container">
             <div class="row">
@@ -176,45 +176,46 @@
                         <div class="footer__logo">
                             <a href="./index.html"><img src="{{ url('') . '/'}}img/logo.png" alt=""></a>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                            cilisis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt cilisis.</p>
                         <div class="footer__payment">
                             <a href="#"><img src="{{ url('') . '/'}}img/payment/payment-1.png" alt=""></a>
                             <a href="#"><img src="{{ url('') . '/'}}img/payment/payment-2.png" alt=""></a>
                             <a href="#"><img src="{{ url('') . '/'}}img/payment/payment-3.png" alt=""></a>
-                            <a href="#"><img src="{{ url('') . '/'}}img/payment/payment-4.png" alt=""></a>
+                            <a
+
+ href="#"><img src="{{ url('') . '/'}}img/payment/payment-4.png" alt=""></a>
                             <a href="#"><img src="{{ url('') . '/'}}img/payment/payment-5.png" alt=""></a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-3 col-sm-5">
                     <div class="footer__widget">
-                        <h6>Quick links</h6>
+                        <h6>Liên kết nhanh</h6>
                         <ul>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Blogs</a></li>
-                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Giới thiệu</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Liên hệ</a></li>
                             <li><a href="#">FAQ</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-3 col-sm-4">
                     <div class="footer__widget">
-                        <h6>Account</h6>
+                        <h6>Tài khoản</h6>
                         <ul>
-                            <li><a href="#">My Account</a></li>
-                            <li><a href="#">Orders Tracking</a></li>
-                            <li><a href="#">Checkout</a></li>
-                            <li><a href="#">Wishlist</a></li>
+                            <li><a href="#">Tài khoản của tôi</a></li>
+                            <li><a href="#">Theo dõi đơn hàng</a></li>
+                            <li><a href="#">Thanh toán</a></li>
+                            <li><a href="#">Danh sách yêu thích</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-8 col-sm-8">
                     <div class="footer__newslatter">
-                        <h6>NEWSLETTER</h6>
+                        <h6>BẢN TIN</h6>
                         <form action="#">
                             <input type="text" placeholder="Email">
-                            <button type="submit" class="site-btn">Subscribe</button>
+                            <button type="submit" class="site-btn">Đăng ký</button>
                         </form>
                         <div class="footer__social">
                             <a href="#"><i class="fa fa-facebook"></i></a>
@@ -228,33 +229,31 @@
             </div>
             <div class="row">
                 <div class="col-lg-12">
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    <!-- Liên kết đến Colorlib không thể bị xóa. Mẫu được cấp phép theo CC BY 3.0. -->
                     <div class="footer__copyright__text">
-                        <p>Copyright &copy;
+                        <p>Bản quyền &copy;
                             <script>
                                 document.write(new Date().getFullYear());
-                            </script> All rights reserved | This template is made with <i
-                                class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com"
-                                target="_blank">Colorlib</a>
+                            </script> Tất cả các quyền được bảo lưu | Mẫu này được làm với <i class="fa fa-heart" aria-hidden="true"></i> bởi <a href="https://colorlib.com" target="_blank">Colorlib</a>
                         </p>
                     </div>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    <!-- Liên kết đến Colorlib không thể bị xóa. Mẫu được cấp phép theo CC BY 3.0. -->
                 </div>
             </div>
         </div>
     </footer>
-    <!-- Footer Section End -->
+    <!-- Kết thúc Phần Footer -->
 
-    <!-- Search Begin -->
+    <!-- Bắt đầu Tìm kiếm -->
     <div class="search-model">
         <div class="h-100 d-flex align-items-center justify-content-center">
             <div class="search-close-switch">+</div>
             <form class="search-model-form">
-                <input type="text" id="search-input" placeholder="Search here.....">
+                <input type="text" id="search-input" placeholder="Tìm kiếm tại đây.....">
             </form>
         </div>
     </div>
-    <!-- Search End -->
+    <!-- Kết thúc Tìm kiếm -->
 
     <!-- Js Plugins -->
     <script src="{{ url('') . '/'}}js/jquery-3.3.1.min.js"></script>
