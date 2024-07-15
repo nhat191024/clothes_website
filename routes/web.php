6fb3c2fa22d75de2d306431599324b04f3514af5
Fumino The Fish <richberchannel01@gmail.com>
2024-07-15 14:56:04 +0700

    Revert "Feature/ntl1997/create contact"

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Http\Controllers\Client\ContactController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     // return view('client.home');
-// });
-Route::prefix('contact')->group(function (){
-    Route::get('/',[ContactController::class, 'index'])->name('contact.index');
+Route::get('/', function () {
+    return view('welcome');
 });
