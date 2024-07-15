f06c727695344c6d88100c61e7ed650b1dd62d1e
ntl1997 <luandajk4@gmail.com>
2024-07-15 20:13:15 +0700

    Update ContactService Create function: storeCustomerRequest and prepare

--- app/Service/client/ContactService.php
+++ app/Service/client/ContactService.php
@@ -2,8 +2,21 @@
 
 namespace App\Service\Client;
 use App\Models\ContactUs;
+use App\Models\CustomerRequest;
+
 class ContactService {
     public function getAll(){
         return ContactUs::first();
+    }
+    public function storeCustomerRequest($data){
+        return CustomerRequest::create($data);
+    }
+    public function prepare($request){
+        return [
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'message' => $request->message,
+        ];
     }
 }
