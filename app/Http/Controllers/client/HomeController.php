e16671154087b5448473ed98574d04b1e0bc1c9e
letnp1024 <letnp1024@gmail.com>
2024-07-15 21:05:22 +0700

    Sửa phần countdown lấy từ database. banner fix : homeServiece, homeController,index.blade.php,main.js

--- app/Http/Controllers/client/HomeController.php
+++ app/Http/Controllers/client/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\client;
 
 use App\Http\Controllers\Controller;
 use App\Service\client\HomeService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -18,7 +19,19 @@ class HomeController extends Controller
         $newProductInfo = $this->homeService->newProducts();
         $trendProductInfo = $this->homeService->trendProduct();
         $favoriteProductInfo = $this->homeService->favoriteProduct();
-        return view('client.home.index', compact('newProductInfo', 'trendProductInfo', 'favoriteProductInfo'));
-        // dd($trendProductInfo);
+        $productAmount = collect($this->homeService->productAmount());
+        $collectionBanner = $this->homeService->collectionBanner();
+
+        $latestPromotion = $this->homeService->latestPromotion();
+        if ($latestPromotion) {
+            $start_time = Carbon::parse($latestPromotion->start_time);
+            $end_time = Carbon::parse($latestPromotion->end_time);
+        }
+        $latestPromotion->daysDiff = $end_time->day - $start_time->day;
+        $latestPromotion->hoursDiff = $end_time->hour - $start_time->hour;
+        $latestPromotion->minsDiff = $end_time->minute - $start_time->minute;
+        $latestPromotion->secsDiff = $end_time->second - $start_time->second;
+        return view('client.home.index', compact('newProductInfo', 'trendProductInfo', 'favoriteProductInfo', 'latestPromotion', 'productAmount', 'collectionBanner'));
+        // dd($collectionBanner);
     }
 }
