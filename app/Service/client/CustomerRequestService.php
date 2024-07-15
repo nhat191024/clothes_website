540291fbb0773ff4b47079462916cefdf7308d84
ntl1997 <luandajk4@gmail.com>
2024-07-15 20:11:56 +0700

    Delete CustomerRequestService

--- app/Service/client/CustomerRequestService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Service\Client;
-use App\Models\CustomerRequest;
-class CustomerRequestService {
-    public function storeCustomerRequest($data){
-        return CustomerRequest::create($data);
-    }
-}
