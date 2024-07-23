03adc7b0c9b078760e49d92fa209d22b9c83f610
HuyLee <huyw300@gmail.com>
2024-07-23 16:52:15 +0700

    Create : Account Detail page

---
+++ resources/views/AccountManagement/AccountManagement.blade.php
@@ -0,0 +1,23 @@
+@extends('client.layout')
+@section('main')
+    <div class="container">
+
+    <div class="footer__newslatter">
+        <form action="#">
+            <aside>
+            h2
+            </aside>
+               <div>
+            <h5>Name</h5>
+            <input type="text" name="" placeholder="Enter name">
+            <h5>Email</h5>
+            <input type="text" name="" placeholder="Enter Email">
+            <h5>Address</h5>
+            <input type="text" name="" placeholder="Enter Address">
+            <h5>Phone</h5>
+            <input type="number" name="" placeholder="Enter Phonenumber">
+        </form>
+    </div>
+    </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('client.home');
 });
+Route::get('/AccountManagement', function () {
+    return view('AccountManagement.AccountManagement');
+});
