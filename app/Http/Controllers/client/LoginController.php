27355b763e6e00f524cd28e7de50490678136619
Catty <ducvu73@gmail.com>
2024-07-29 13:18:52 +0700

    feat: Add login and logout functionality
    This commit introduces new controllers for handling client login and logout operations. The `LoginController` utilizes a dedicated `LoginService` to manage authentication and provides routes for displaying the login page, processing login requests, and logging out users.

---
+++ app/Http/Controllers/client/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\client;
+
+use App\Http\Controllers\Controller;
+use App\Service\client\LoginService;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    private $loginService;
+    public function __construct(LoginService $loginService)
+    {
+        $this->loginService = $loginService;
+    }
+    public function index(){
+        return view('client.login.login');
+    }
+
+    public function login(Request $request){
+        return $this->loginService->loginAuth($request);
+    }
+
+    public function logout(Request $request){
+        return $this->loginService->logout($request);
+    }
+}
+
+
