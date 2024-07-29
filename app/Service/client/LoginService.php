808c8f8e328a9805c5be65125bb1c0dab506f615
Catty <ducvu73@gmail.com>
2024-07-29 13:18:59 +0700

    feat: Implement client login functionality
    Adds a new service to handle user login and logout actions. Includes validation for username and password, and redirects to appropriate routes based on login status.

---
+++ app/Service/client/LoginService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Service\client;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class LoginService
+{
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('home.index');
+    }
+
+    public function loginAuth(Request $request)
+    {
+        $request->validate(
+            [
+                'username' => ['required', 'string'],
+                'password' => ['required', 'string'],
+            ],
+            [
+                'username.required' => 'Username, email, SĐT không được để trống!',
+                'password.required' => 'Mật khẩu không được để trống!'
+            ]
+        );
+
+        $usernameInput = $request->input('username');
+        $typeOfInputValue = filter_var($usernameInput, FILTER_VALIDATE_EMAIL) ? 'email'
+            : (is_numeric($usernameInput) ? 'phone' : 'username');
+
+        $loginData = [
+            $typeOfInputValue => $usernameInput,
+            'password' => $request->input('password')
+        ];
+
+        if (!Auth::attempt($loginData)) {
+            return redirect()->route('client.login.index')->with('message', 'Thông tin không hợp lệ!');
+        }
+        /** @var \App\Models\User $user **/  $user = Auth::user();
+        if ($user->status == 0) {
+            return redirect()->route('client.login.index')->with('message', 'Người dùng đã bị khoá!');
+        }
+        return redirect()->intended(route('home.index'));
+    }
+}
+
