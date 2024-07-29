67ea57a54e386cf452de2c8bcb6194e7593213cb
Catty <ducvu73@gmail.com>
2024-07-29 13:39:42 +0700

    feat: Redirect authenticated users to homepage
    Prevents logged-in clients from accessing the login page, redirecting them to the homepage instead. This enhances user experience by providing a seamless transition after successful login.

--- app/Http/Controllers/client/LoginController.php
+++ app/Http/Controllers/client/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\client;
 use App\Http\Controllers\Controller;
 use App\Service\client\LoginService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -14,6 +15,9 @@ class LoginController extends Controller
         $this->loginService = $loginService;
     }
     public function index(){
+        if(Auth::check()){
+            return redirect()->route('home.index');
+        }
         return view('client.login.login');
     }
 
