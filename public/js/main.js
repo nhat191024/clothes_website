c4667c8405a089bc0bc05cdf364a4fb6447493bb
Catty <ducvu73@gmail.com>
2024-07-29 15:37:10 +0700

    feat: Add hover functionality to dropdowns
    Adds hover functionality to dropdown menus, making them more user-friendly and visually appealing. When hovering over a dropdown, its menu will now be displayed immediately.

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,17 @@ Created: Colorib
 
 "use strict";
 
+$(document).ready(function() {
+    $('.dropdown').hover(function() {
+        $(this).addClass('show');
+        $(this).find('.dropdown-menu').addClass('show');
+    }, function() {
+        $(this).removeClass('show');
+        $(this).find('.dropdown-menu').removeClass('show');
+    });
+});
+
+
 (function ($) {
     /*------------------
         Preloader
