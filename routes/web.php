bd240ebb4a709cae13dac6e0cbc5e009633991a1
Catty <ducvu73@gmail.com>
2024-07-29 13:19:21 +0700

    feat: Add user authentication routes
    Introduce user login and logout functionality. The new routes allow users to access a dedicated login page, authenticate their credentials, and log out upon completion.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\client\ContactController;
+use App\Http\Controllers\client\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\client\HomeController;
 
@@ -11,5 +12,11 @@ Route::prefix('/')->group(function () {
 Route::prefix('contact')->group(function () {
     Route::get('/', [ContactController::class, 'index'])->name('client.contact.index');
     Route::post('/', [ContactController::class, 'store'])->name('customer.requests.store');
+});
+
+Route::prefix('user')->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('client.login.index');
+    Route::get('/logout', [LoginController::class, 'logout'])->name('client.logout');
+    Route::post('/login/check', [LoginController::class, 'login'])->name('client.login');
 });
 
