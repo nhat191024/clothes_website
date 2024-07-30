794804eb3014c3e2f2bebf901a7a7a852f36cf55
letnp1024 <letnp1024@gmail.com>
2024-07-30 11:57:08 +0700

    thêm productCategory trong model.products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -28,10 +28,12 @@ class Product extends Model
     {
         return $this->hasMany(ProductDetail::class);
     }
+    // thêm productCategory vì dữ liệu lọc ra bị sai khi dùng categories
     public function productCategory()
     {
         return $this->hasMany(ProductCategory::class);
     }
+
     public function categories()
     {
         return $this->belongsToMany(Category::class, 'product_categories', 'category_id', 'product_id');
