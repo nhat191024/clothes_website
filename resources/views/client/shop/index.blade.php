4ad4bcde0bfd862c5efa80c6665ebae45fc915a5
Catty <ducvu73@gmail.com>
2024-08-02 11:34:45 +0700

    feat: Implement shop filtering
    Adds filtering functionality to the shop page, allowing customers to narrow down products by category, price, size, and color. This enhances user experience by providing a more targeted and efficient shopping journey.
    
    The implementation includes:
    
    - A filter sidebar with checkboxes for selecting categories, sizes, and colors.
    - A price range slider for filtering products within a specific price range.
    - JavaScript functionality to dynamically update the product list based on selected filter criteria.
    
    This feature introduces a more engaging shopping experience for customers, making it easier for them to find the desired products.

--- resources/views/client/shop/index.blade.php
+++ resources/views/client/shop/index.blade.php
@@ -1,8 +1,8 @@
 @extends('client.layout')
 @section('main')
  <!-- Shop Section Begin -->
- 
  <section class="shop spad">
+
     <div class="container">
         <div class="row">
             <div class="col-lg-3 col-md-3">
@@ -13,16 +13,24 @@
                         </div>
                         <div class="categories__accordion">
                             <div class="accordion" id="accordionExample">
-                                @foreach($parentCategory as $pCateg)
+                                @foreach($allCategories as $pCateg)
+                                @if ($loop->index == 4) @break @endif
                                 <div class="card">
-                                    <div class="card-heading active">
-                                        <a data-toggle="collapse" data-target="#collapseOne">{{ $pCateg->name }}</a>
+                                    <div class="card-heading">
+                                        <a data-toggle="collapse" data-target="#collapse-{{ $pCateg->id }}">{{ $pCateg->name }}</a>
                                     </div>
-                                    <div id="collapseOne" class="collapse show" data-parent="#accordionExample">
+                                    <div id="collapse-{{ $pCateg->id }}" class="collapse" data-parent="#accordionExample">
                                         <div class="card-body" >
-                                            <ul >
-                                                @foreach($childCategory as $cCateg)
-                                                <li><a href="#" data-ParentCategory="{{ $pCateg->id }}" data-ChildCategory="{{ $cCateg->id }}" >{{ $cCateg->name }}</a></li>
+                                            <ul id="categoryFilterItems">
+                                                @foreach($allCategories as $cCateg)
+                                                @if ($loop->index < 4) @continue @endif
+                                                <li>
+                                                    <label for="{{ $pCateg->id }}-{{ $cCateg->id }}">
+                                                        <input type="checkbox" id="{{ $pCateg->id }}-{{ $cCateg->id }}" class="category-filter pointer" data-ParentCategory="{{ $pCateg->id }}" value="{{ $cCateg->id }}">
+                                                        {{ $cCateg->name }}
+                                                        <span class="checkmark"></span>
+                                                    </label>
+                                                </li>
                                                 @endforeach
                                             </ul>
                                         </div>
@@ -38,7 +46,8 @@
                         </div>
                         <div class="filter-range-wrap">
                             <div class="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"
-                            data-min="{{$minPrice}}" data-max="{{$maxPrice}}"></div>
+                            data-min="{{$allProducts->min('price')}}" data-max="{{$allProducts->max('price')}}"
+                            ></div>
                             <div class="range-slider">
                                 <div class="price-input">
                                     <p>Price:</p>
@@ -46,15 +55,17 @@
                                     <input type="text" id="maxamount">
                                 </div>
                             </div>
+                            <a class="pointer" id="filter">Apply Filter</a>
+                            <a class="pointer hidden" id="removeFilter">Reset</a>
                         </div>
-                        <a href="#">Filter</a>
+
                     </div>
                     <div class="sidebar__sizes">
                         <div class="section-title">
                             <h4>Shop by size</h4>
                         </div>
                         <div class="size__list">
-                            @foreach($sizes as $size)
+                            @foreach($allSizes as $size)
                             <label for="{{ $size->name }}">
                                 {{ $size->name }}
                                 <input type="checkbox" id="{{ $size->name }}" class="size-filter" value="{{ $size->id }}">
@@ -68,20 +79,24 @@
                             <h4>Shop by color</h4>
                         </div>
                         <div class="size__list color__list">
-                            @foreach($colors as $color)
-                            <label for="{{ $color->name }}">
+                            @foreach($allColors as $index => $color)
+                            <label for="{{ $color->name }}" class="color-item {{ $loop->index >= 5 ? 'hidden' : '' }}">
                                 {{ $color->name }}
                                 <input type="checkbox" id="{{ $color->name }}" class="color-filter" value="{{ $color->id }}">
                                 <span class="checkmark"></span>
                             </label>
                             @endforeach
                         </div>
+                        @if(count($allColors) > 5)
+                            <a id="see-more" class="pointer">More colors</a>
+                        @endif
                     </div>
+
                 </div>
             </div>
             <div class="col-lg-9 col-md-9">
-                <div class="row" id="productA-list">
-                    @foreach($allProducts as $product)
+                <div class="row" id="products-list">
+                    @foreach($allPageProducts as $product)
                     <div class="col-lg-4 col-md-6">
                         <div class=".product__item">
                             <div class="product__item__pic set-bg" data-setbg="{{ $product->img }}">
@@ -101,16 +116,19 @@
                                     <i class="fa fa-star"></i>
                                     <i class="fa fa=star"></i>
                                 </div>
-                                <div class="product__price">{{ $product->price }}</div>
+                                <div class="product__price">¥{{ $product->price }}</div>
                             </div>
                         </div>
                     </div>
                     @endforeach
-                    {{ $allProducts->links('client.shop.paginate') }}
+                    {{ $allPageProducts->links('client.shop.paginate') }}
                 </div>
             </div>
         </div>
     </div>
+    <link rel="stylesheet" href="{{ asset('css/shop/shop.css') }}"></link>
+    <script src="{{ url('') . '/'}}js/jquery-3.3.1.min.js"></script>
+    <script src="{{ url('') . '/'}}js/shop/shop.js"></script>
 </section>
 <!-- Shop Section End -->
 @endsection
