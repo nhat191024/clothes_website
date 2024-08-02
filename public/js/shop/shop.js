7f9e6f029aff033020cc5037b40ee0f64356741a
Catty <ducvu73@gmail.com>
2024-08-02 11:32:20 +0700

    feat: Implement product filtering and sorting
    Adds functionality to filter and sort products based on categories, colors, sizes, and price range. This allows users to easily find the products they are looking for.
    
    The new functionality uses AJAX to dynamically update the product list without requiring a full page reload. This improves the user experience by making the filtering process more seamless.

---
+++ public/js/shop/shop.js
@@ -0,0 +1,112 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const seeMoreButton = document.getElementById('see-more');
+    if (seeMoreButton) {
+        seeMoreButton.addEventListener('click', function () {
+            const hiddenItems = document.querySelectorAll('.color-item.hidden');
+            hiddenItems.forEach(item => {
+                item.classList.remove('hidden');
+            });
+            seeMoreButton.style.display = 'none';
+        });
+    }
+});
+
+$(document).ready(function () {
+    let filterData = {
+        category: [],
+        color: [],
+        size: [],
+        max: null,
+        min: null
+    };
+
+    $('#removeFilter').on('click', function () {
+        window.location.reload();
+    });
+
+    $('#filter').on('click', function () {
+        resetFilter();
+        $('.category-filter:checked').each(function() {
+            filterData.category.push(parseInt($(this).val(), 10));
+            filterData.category.push($(this).data("parentcategory"));
+        });
+        // da line below iz for removin' duplicate IDs lol
+        filterData.category = [...new Set(filterData.category)];
+        filterData.size = getCheckedValues('.size-filter');
+        filterData.color = getCheckedValues('.color-filter');
+        filterData.min = parseCurrency($('#minamount').val());
+        filterData.max = parseCurrency($('#maxamount').val());
+        applyFilters(filterData);
+    });
+
+
+    function applyFilters(filterData) {
+        $.ajax({
+            url: "/shop/filter-products",
+            type: "GET",
+            data: filterData,
+            beforeSend: function () {
+                setFilterButton('Searching...', 'orange', false);
+            },
+            success: function (response) {
+                updateProductList(response);
+                setFilterButton('Filtered', 'green', true);
+                $('#removeFilter').removeClass('hidden');
+            },
+            error: function (xhr, status, error) {
+                setFilterButton('Filter', 'red', true);
+            },
+        });
+    }
+
+    function setFilterButton(text,color,enabled) {
+        $('#filter').text(text);
+        $('#filter').css('border-color', color);
+        $('#filter').prop('disabled', !enabled);
+    }
+
+    function updateProductList(response) {
+        let products = response.data;
+        let productHtml = '';
+        if (products.length === 0) {
+            productHtml = '<p class="col-12 text-center">No products found with the selected filters. <br> Click "RESET" to see all products available.</p>';
+        }
+        products.forEach(product => {
+            productHtml += `
+                <div class="col-lg-4 col-md-6">
+                    <div class=".product__item">
+                        <div class="product__item__pic set-bg" data-setbg="${product.img}" style="background-image: url(&quot;${product.img}&quot;);">
+                            <div class="label new">New</div>
+                            <ul class="product__hover">
+                                <li><a href="img/shop/shop-1.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                                <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                                <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                            </ul>
+                        </div>
+                        <div class="product__item__text">
+                            <h6><a href="#">${product.name}</a></h6>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa=star"></i>
+                            </div>
+                            <div class="product__price">¥${product.price}</div>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+        $('#products-list').html(productHtml);
+    }
+
+    function resetFilter() {
+        filterData.category = [];
+        filterData.color = [];
+        filterData.size = [];
+        filterData.max = null;
+        filterData.min = null;
+    }
+});
