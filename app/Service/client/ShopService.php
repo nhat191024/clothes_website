478114c790e64be8afa6c14941b15bd3b881917b
Catty <ducvu73@gmail.com>
2024-08-02 11:33:13 +0700

    feat: Add product filtering and pagination
    Implement product filtering based on category, color, size, and price range. Introduce pagination for displaying products.  Increase the number of products per page to 18 for improved user experience.

--- app/Service/client/ShopService.php
+++ app/Service/client/ShopService.php
@@ -4,25 +4,55 @@ namespace App\Service\client;
 
 // use App\Models\Banner;
 use App\Models\Category;
+use App\Models\Color;
 // use App\Models\Color;
 // use App\Models\Size;
 use App\Models\Product;
+use App\Models\Size;
 // use App\Models\Promotion;
 use Illuminate\Support\Facades\DB;
 
 class ShopService
 {
+    public function getAllPaginatedProducts($countPerPage)
+    {
+        return Product::paginate($countPerPage);
+    }
     public function getAllProducts()
     {
-        return Product::paginate(9);
+        return Product::all();
+    }
+    public function getAllCategories()
+    {
+        return Category::all();
+    }
+    public function getAllSizes()
+    {
+        return Size::all();
+    }
+
+    public function getAllColors()
+    {
+        return Color::all();
     }
 
     public function getProductsByFilters($categoryIds, $colorIds, $sizeIds, $maxPrice, $minPrice)
     {
         $mainQuery = Product::query();
+        $mainCategoryIds = collect($categoryIds)->filter(function ($id) {
+                return $id >= 1 && $id <= 4;
+        })->toArray();
 
-        $categoryIds == null ? true : $mainQuery->whereHas('categories', function ($query) use ($categoryIds) {
-            $query->where('category_id', $categoryIds);
+        $subCategoryIds = collect($categoryIds)->filter(function ($id) {
+                return $id >= 5 && $id <= 8;
+        })->toArray();
+
+        $subCategoryIds == null ? true : $mainQuery->whereHas('categories', function ($query) use ($subCategoryIds) {
+            $query->whereIn('category_id', $subCategoryIds);
+        });
+
+        $mainCategoryIds == null ? true : $mainQuery->whereHas('categories', function ($query) use ($mainCategoryIds) {
+            $query->whereIn('category_id', $mainCategoryIds);
         });
 
         $colorIds == null ? true :  $mainQuery->whereHas('productDetail', function ($query) use ($colorIds) {
@@ -33,10 +63,10 @@ class ShopService
             $query->whereIn('size_id', $sizeIds);
         });
 
-        $maxPrice == null ? true : $mainQuery->where('price', '>=', 0);
+        $maxPrice == null ? true : $mainQuery->where('price', '<=', $maxPrice);
 
-        $minPrice == null ? true : $mainQuery->where('price', '<=', 0);
+        $minPrice == null ? true : $mainQuery->where('price', '>=', $minPrice);
 
-        return $mainQuery->paginate(9);
+        return $mainQuery->paginate(18);
     }
 }
