c69fffd4642c46d8ae15c78e214d5259d25ec7cf
Catty <ducvu73@gmail.com>
2024-08-06 10:51:19 +0700

    Fix: Add links to product pages in shop
    The `shop.js` file was missing links to product pages, leading to a broken user experience. This commit fixes the issue by adding links to product pages using the product ID.
    
    This change enhances user experience and allows customers to access product details.

--- public/js/shop/shop.js
+++ public/js/shop/shop.js
@@ -31,7 +31,6 @@ $(document).ready(function () {
             filterData.category.push(parseInt($(this).val(), 10));
             filterData.category.push($(this).data("parentcategory"));
         });
-        // da line below iz for removin' duplicate IDs lol
         filterData.category = [...new Set(filterData.category)];
         filterData.size = getCheckedValues('.size-filter');
         filterData.color = getCheckedValues('.color-filter');
@@ -80,12 +79,11 @@ $(document).ready(function () {
                             <div class="label new">New</div>
                             <ul class="product__hover">
                                 <li><a href="img/shop/shop-1.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
-                                <li><a href="#"><span class="icon_heart_alt"></span></a></li>
-                                <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                                <li><a href="/shop/product/${product.id}"><span class="icon_bag_alt"></span></a></li>
                             </ul>
                         </div>
                         <div class="product__item__text">
-                            <h6><a href="#">${product.name}</a></h6>
+                            <h6><a href="/shop/product/${product.id}">${product.name}</a></h6>
                             <div class="rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
