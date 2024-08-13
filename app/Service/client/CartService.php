51bcca489d8e7d189b6c55063bed68076fb8dcda
Catty <ducvu73@gmail.com>
2024-08-13 14:32:45 +0700

    feat: Add cart functionality
    Adds functionality to the cart service, allowing users to add items to their cart, view their cart contents, and remove items from their cart. Also implements cart persistence for logged-in users.

--- app/Service/client/CartService.php
+++ app/Service/client/CartService.php
@@ -3,22 +3,67 @@
 namespace App\Service\client;
 
 use App\Models\Cart;
+use App\Models\ProductDetail;
 use Illuminate\Support\Facades\Auth;
 
 class CartService
 {
-    public function getAll()
-    {
-        return Cart::all();
-    }
 
     public function getCart()
     {
-        return Cart::where('user_id', Auth::user()->id)->get();
+        $user = Auth::user();
+        if ($user) {
+            return Cart::where('user_id', $user->id)->get();
+        }
+        return $this->getCartSession();
     }
 
     public function storeCart($data)
     {
-        return Cart::create($data);
+        $productDetail = $this->getProductDetail($data['product_id'], $data['color_id'], $data['size_id']);
+        $cartUser = Cart::where('product_detail_id', $productDetail->id)->where('user_id', Auth::user()->id);
+        $isDuplicate = $cartUser->exists();
+        if($isDuplicate){
+            return $this->updateQuantity($productDetail->id, $data['quantity'] + $cartUser->first()->quantity);
+        }
+        Cart::create(
+            [
+                'product_detail_id' => $productDetail->id,
+                'user_id' => Auth::user()->id,
+                'quantity' => $data['quantity'],
+                'price' => $data['price'],
+            ]
+        );
+        return [
+            'status' => 200,
+            'success' => true
+        ];
+    }
+
+    public function getProductDetail($productId, $colorId, $sizeId)
+    {
+        return ProductDetail::where('product_id', $productId)
+            ->where('color_id', $colorId)
+            ->where('size_id', $sizeId)
+            ->first();
+    }
+
+    public function removeProductByDetailId($productDetailId)
+    {
+        Cart::where('product_detail_id', $productDetailId)->delete();
+        return 'ok';
+    }
+
+    public function updateQuantity($productDetailId, $quantity)
+    {
+        Cart::where('product_detail_id', $productDetailId)->update([
+            'quantity' => $quantity
+        ]);
+        return 'ok';
+    }
+
+    public function getCartSession()
+    {
+        return null;
     }
 }
