05ab8066f0186e8d94d2168aa6cd6e4f37059e94
Catty <ducvu73@gmail.com>
2024-08-14 18:11:23 +0700

    feat: Implement cart functionality for logged-out users
    Adds functionality to allow users who are not logged in to use the cart. The changes introduce a new `CartSessionService` and conditionally use either `CartService` or `CartSessionService` based on the user's login status. This allows for a consistent cart experience regardless of user authentication.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,43 +2,59 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
 use App\Service\client\CartService;
+use App\Service\client\CartSessionService;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
     private $cartService;
-    public function __construct(CartService $cartService)
+    private $cartSessionService;
+    private $isLoggedIn;
+    private $cart;
+    public function __construct(CartService $cartService,CartSessionService $cartSessionService)
     {
         $this->cartService = $cartService;
+        $this->cartSessionService = $cartSessionService;
+        $this->isLoggedIn = Auth::check();
+        $cart = ($this->isLoggedIn) ?
+            $this->cartService->getCart() :
+                $this->cartSessionService->getCart();
+        $this->cart = $cart;
     }
 
     public function index()
     {
         return view('client.cart.cart')->with([
-            'cart' => $this->cartService->getCart() ?: []
+            'cart' => $this->cart
         ]);
     }
 
     public function getCart()
     {
-        return $this->cartService->getCart();
+        return $this->cart;
     }
 
     public function addToCart(Request $request)
     {
-        return $this->cartService->storeCart($request->all());
+        return ($this->isLoggedIn) ?
+            $this->cartService->storeCart($request->all()) :
+                $this->cartSessionService->storeCart($request->all());
     }
 
     public function removeFromCart(Request $request)
     {
-        return $this->cartService->removeProductByDetailId($request->product_detail_id);
+        return ($this->isLoggedIn) ?
+            $this->cartService->removeProductByDetailId($request->product_detail_id) :
+                $this->cartSessionService->removeProductByDetailId($request->product_detail_id);
     }
 
     public function updateQuantity(Request $request)
     {
-        return $this->cartService->updateQuantity($request->product_detail_id, $request->quantity);
+        return ($this->isLoggedIn) ?
+            $this->cartService->updateQuantity($request->product_detail_id, $request->quantity) :
+                $this->cartSessionService->updateQuantity($request->product_detail_id, $request->quantity);
     }
 
 }
