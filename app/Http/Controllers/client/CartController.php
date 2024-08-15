4b53d914862cb7c301adec59c583dba0c5e0cbc5
Catty <ducvu73@gmail.com>
2024-08-15 23:10:20 +0700

    feat: Implement cart functionality
    Adds a new `CartController` to handle cart operations for both logged-in and session-based users. The controller provides functionality for adding, removing, updating items in the cart, applying vouchers, and retrieving cart details.

---
+++ app/Http/Controllers/client/CartController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\client;
+
+use App\Http\Controllers\Controller;
+use App\Service\client\VoucherService;
+use Illuminate\Support\Facades\Auth;
+use App\Service\client\CartService;
+use App\Service\client\CartSessionService;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    private $cartService;
+    private $cartSessionService;
+    private $voucherService;
+    private $isLoggedIn;
+    private $cart;
+    public function __construct(CartService $cartService,CartSessionService $cartSessionService,VoucherService $voucherService)
+    {
+        $this->cartService = $cartService;
+        $this->cartSessionService = $cartSessionService;
+        $this->voucherService = $voucherService;
+        $this->isLoggedIn = Auth::check();
+        $cart = ($this->isLoggedIn) ?
+            $this->cartService->getCart() :
+                $this->cartSessionService->getCart();
+        $this->cart = $cart;
+    }
+
+    public function index()
+    {
+        $subtotal = ($this->isLoggedIn) ?
+            $this->cartService->getSubtotal() :
+                $this->cartSessionService->getSubtotal();
+        $appliedVoucher = $this->voucherService->getActivatedVoucher($subtotal);
+        return view('client.cart.cart')->with([
+            'cart' => $this->cart,
+            'subtotal' => $subtotal,
+            'total' => $subtotal - ($subtotal * ($appliedVoucher ? $appliedVoucher->discount_percentage : 0) / 100),
+            'voucher' => $appliedVoucher
+        ]);
+    }
+
+    public function getCart()
+    {
+        return $this->cart;
+    }
+
+    public function addToCart(Request $request)
+    {
+        return ($this->isLoggedIn) ?
+            $this->cartService->storeCart($request->all()) :
+                $this->cartSessionService->storeCart($request->all());
+    }
+
+    public function removeFromCart(Request $request)
+    {
+        return ($this->isLoggedIn) ?
+            $this->cartService->removeProductByDetailId($request->product_detail_id) :
+                $this->cartSessionService->removeProductByDetailId($request->product_detail_id);
+    }
+
+    public function updateQuantity(Request $request)
+    {
+        return ($this->isLoggedIn) ?
+            $this->cartService->updateQuantity($request->product_detail_id, $request->quantity) :
+                $this->cartSessionService->updateQuantity($request->product_detail_id, $request->quantity);
+    }
+
+    public function applyVoucher(Request $request)
+    {
+        $subtotal = ($this->isLoggedIn) ?
+            $this->cartService->getSubtotal() :
+                $this->cartSessionService->getSubtotal();
+        return $this->voucherService->applyVoucher($request->input('voucher_code'), $subtotal);
+    }
+}
