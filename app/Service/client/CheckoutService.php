a5e9d7872bf29d399c01ede4ebf0b50176a3d25e
nhat191024 <richberchannel01@gmail.com>
2024-08-16 18:21:09 +0700

    Refactor: CheckoutService to use CartSessionService for cart retrieval

--- app/Service/client/CheckoutService.php
+++ app/Service/client/CheckoutService.php
@@ -2,39 +2,60 @@
 
 namespace App\Service\client;
 
-use App\Models\Cart;
 use App\Models\User;
 use App\Models\Bill;
 use App\Models\BillDetail;
 use App\Service\client\CartService;
+use App\Service\client\CartSessionService;
+use App\Service\client\VoucherService;
 use App\Service\MailService;
 use Illuminate\Support\Facades\Auth;
 
 class CheckoutService
 {
     private $cartService;
+    private $cartSessionService;
+    private $voucherService;
     private $mailService;
+
+    private $carts;
 
     public function __construct()
     {
         $this->cartService = new CartService();
+        $this->cartSessionService = new CartSessionService();
         $this->mailService = new MailService();
+        $this->voucherService = new VoucherService();
+
+        $this->carts = $this->cartService->getCart();
+        if ($this->carts == null) $this->carts = $this->cartSessionService->getCart();
     }
 
     public function getProductList()
     {
-        $carts = $this->cartService->getCart();
-        return $carts;
+        return $this->carts;
+    }
+
+    public function getDiscount()
+    {
+        $voucher = $this->voucherService->getDiscountAmount($this->getCartSubTotal());
+        return $voucher;
+    }
+
+    public function getCartSubTotal()
+    {
+        $total = 0;
+        foreach ($this->carts as $cart) {
+            $total += $cart['price'] * $cart['quantity'];
+        }
+        return $total;
     }
 
     public function getCartTotal()
     {
-        $carts = $this->cartService->getCart();
-        $total = 0;
-        foreach ($carts as $cart) {
-            $total += $cart->price * $cart->quantity;
-        }
-        return $total;
+        $total = $this->getCartSubTotal();
+        $discount = $this->getDiscount();
+        return $total - $discount;
     }
 
     public function confirmOrder($request)
@@ -44,9 +65,16 @@ class CheckoutService
         $point = $total / 100;
         $buildingName = $request->buildingName === 'null' ? '' : ', ' . $request->buildingName;
         $address = $request->prefecture . ', ' . $request->city . ', ' . $request->address . $buildingName;
-        User::where('id', $user->id)->update([
-            'point' => $user->point + $point
-        ]);
+
+        if ($user) {
+            User::where('id', $user->id)->update([
+                'point' => $user->point + $point
+            ]);
+        } else {
+            $point = 0;
+        }
+
+
 
         $bill = Bill::create([
             'full_name' => $request->fullName,
@@ -69,7 +97,8 @@ class CheckoutService
             $request->payment,
             $request->delivery,
             $bill->created_at,
-            0,
+            $this->getCartSubTotal(),
+            $this->getDiscount(),
             $total
         );
 
@@ -83,22 +112,25 @@ class CheckoutService
             $request->payment,
             $request->delivery,
             $bill->created_at,
-            0,
+            $this->getCartSubTotal(),
+            $this->getDiscount(),
             $total
         );
 
-        $carts = $this->getProductList();
-        foreach ($carts as $cart) {
+        foreach ($this->carts as $cart) {
             BillDetail::create([
                 'bill_id' => $bill->id,
-                'product_id' => $cart->productDetail->product->id,
-                'color_id' => $cart->productDetail->color->id,
-                'size_id' => $cart->productDetail->size->id,
-                'quantity' => $cart->quantity,
-                'price' => $cart->price,
+                'product_id' => $cart['productDetail']->product->id,
+                'color_id' => $cart['productDetail']->color->id,
+                'size_id' => $cart['productDetail']->size->id,
+                'quantity' => $cart['quantity'],
+                'price' => $cart['price'],
             ]);
-            $this->cartService->removeProductByDetailId($cart->product_detail_id);
+
+            $user ? $this->cartService->removeProductByDetailId($cart['product_detail_id']) : $this->cartSessionService->removeProductByDetailId($cart['product_detail_id']);
         }
+
+        $this->voucherService->clearVoucher();
 
         return response()->json([
             'status' => 200,
