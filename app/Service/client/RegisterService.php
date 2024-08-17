877b87bdd694296878a26470b340893fc3fd395f
HuyLee <huyw300@gmail.com>
2024-08-17 18:11:28 +0700

    Create : RegisterService

--- app/Service/client/RegisterService.php
+++ app/Service/client/RegisterService.php
@@ -2,32 +2,14 @@
 
 namespace App\Service\client;
 
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 
 class RegisterService {
-    public function registerAuth(Request $request){
-        $request->validate([
-            'username' => ['required','max:191','unique:users'],
-            'name' => ['required','string','max:191'],
-            'phone' => ['required','string','max:191','unique:users'],
-            'email' => ['required','string','email','max:191','unique:users'],
-            'password' => ['required','string','min:8','confirmed'],
-        ],         
-        [
-            'username.required' => 'Username is required!',
-            'username.unique' => 'Username is already taken!',
-            'name.required' => 'Name is required!',
-            'phone.required' => 'Phone number is required!',
-            'phone.unique' => 'Phone number is already taken!',
-            'email.required' => 'Email is required!',
-            'email.unique' => 'Email is already taken!',
-            'password.required' => 'Password is required!',
-            'password.min' => 'Password must be at least 8 characters!',
-            'password.confirmed' => 'Password confirmation does not match!',
-        ]
-    );
+    
+    public function registerAuth(RegisterRequest $request){
+
         $user = User::create([
             'username' =>$request->username,
             'full_name' => $request->name,
