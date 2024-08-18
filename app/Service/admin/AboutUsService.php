dc2688f825ce0f07aabe07043065283268d28ab4
Catty <ducvu73@gmail.com>
2024-08-18 14:22:11 +0700

    Refactor: Use ContactInfo model for About Us

--- app/Service/admin/AboutUsService.php
+++ app/Service/admin/AboutUsService.php
@@ -2,32 +2,27 @@
 
 namespace App\Service\admin;
 
-use App\Models\About_us;
 use App\Models\Banners;
+use App\Models\ContactInfo;
 
 class AboutUsService
 {
     public function getAll()
     {
-        $about = About_us::first();
+        $about = ContactInfo::first();
         return $about;
     }
 
     public function getById($id)
     {
-        return About_us::where('id', $id)->first();
+        return ContactInfo::where('id', $id)->first();
     }
 
-    public function edit($id, $aboutTitle, $aboutTitleEn, $aboutContent, $aboutContentEn, $imageName)
+    public function edit($id, $name, $type)
     {
-        $about = About_us::where('id', $id)->first();
-        $about->title = $aboutTitle;
-        $about->title_en = $aboutTitleEn;
-        $about->description = $aboutContent;
-        $about->description_en = $aboutContentEn;
-        if ($imageName != null) {
-            $about->image = $imageName;
-        }
+        $about = ContactInfo::where('id', $id)->first();
+        $about->name = $name;
+        $about->type = $type;
         $about->save();
     }
 }
