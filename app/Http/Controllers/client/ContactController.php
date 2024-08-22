713d834c153420988de2394620db0bfab10ffbd6
nhat191024 <richberchannel01@gmail.com>
2024-08-22 15:28:45 +0700

    Refactor: ContactController for better readability and dependency injection

--- app/Http/Controllers/client/ContactController.php
+++ app/Http/Controllers/client/ContactController.php
@@ -4,21 +4,25 @@ namespace App\Http\Controllers\client;
 
 use App\Http\Controllers\Controller;
 use App\Service\Client\ContactService;
-use Illuminate\Http\Request;
 use App\Http\Requests\StoreCustomerMessageRequest;
 
 class ContactController extends Controller
 {
     private $contactService;
 
-    public function __construct(ContactService $contactService) {
-        $this->contactService = $contactService;
+    public function __construct()
+    {
+        $this->contactService = app(contactService::class);
     }
-    public function index(){
+
+    public function index()
+    {
         $contactUs = $this->contactService->getAll();
         return view('client.contact.index', compact('contactUs'));
     }
-    public function store(StoreCustomerMessageRequest $request){
+
+    public function store(StoreCustomerMessageRequest $request)
+    {
         $data = $this->contactService->prepare($request);
         $this->contactService->storeCustomerRequest($data);
         return redirect()->back();
