f905e35dae0da8c4ea051e35ed42efab3aec453e
nhat191024 <richberchannel01@gmail.com>
2024-08-22 16:05:11 +0700

    Refactor

--- app/Http/Controllers/admin/BillController.php
+++ app/Http/Controllers/admin/BillController.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 class BillController extends Controller
 {
     private $billService;
-    //
+
     public function __construct(BillService $billService)
     {
         $this->billService = $billService;
