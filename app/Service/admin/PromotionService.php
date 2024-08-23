a313415880f5e97b32b3a9dc1ef671bc7c29cbda
Catty <ducvu73@gmail.com>
2024-08-23 20:51:19 +0700

    feat: Implement Promotion edit functionality

--- app/Service/admin/PromotionService.php
+++ app/Service/admin/PromotionService.php
@@ -8,16 +8,30 @@ class PromotionService
 {
     public function get()
     {
-        return Promotion::first();
+        return Promotion::all();
+    }
+    public function getById($id)
+    {
+        return Promotion::find($id);
     }
 
     public function edit($request)
     {
-        $promotion = PromotionService::get();
+        $request->validate([
+            'id' => 'required|exists:promotions,id',
+            'product_id' => 'required|exists:products,id',
+            'description' => 'nullable|string',
+            'discount_percentage' => 'required|numeric|min:0|max:100',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:start_time',
+        ]);
+        $promotion = PromotionService::getById($request->id)->first();
         $promotion->product_id = $request->product_id;
+        $promotion->description = $request->description;
         $promotion->discount_percentage = $request->discount_percentage;
         $promotion->start_time = $request->start_time;
         $promotion->end_time = $request->end_time;
+        // $promotion->id = $request->id;
         return $promotion->save();
     }
 }
