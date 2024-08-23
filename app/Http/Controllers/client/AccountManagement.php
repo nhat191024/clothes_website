3608014af4396afd62ee9e88b75e7700f55ceb95
Catty <ducvu73@gmail.com>
2024-08-23 09:49:12 +0700

    feat: Implement password change and user info update and image upload

--- app/Http/Controllers/client/AccountManagement.php
+++ app/Http/Controllers/client/AccountManagement.php
@@ -20,7 +20,7 @@ class AccountManagement extends Controller
 
         return view('client.Account.ChangePassword', compact('user'));
     }
-    // Chưa sử dụng 
+    // Chưa sử dụng
     public function changePass(Request $request){
         /** @var \App\Models\User $user */
         $user = Auth::user();
@@ -30,10 +30,14 @@ class AccountManagement extends Controller
                 return redirect()->route('client.account.changepassword')
                 ->with('error', 'Current password is incorrect.');
             }
+            $request->validate([
+                'passwordNew'=> 'required|min:8',
+                'passwordConfirm'=> 'required|same:passwordNew',
+            ]);
             if ($data['passwordNew'] == $data['passwordConfirm']) {
                 $user->password = bcrypt($data['passwordNew']);
                 $user->save();
-                return redirect()->route('client.account.index')->with('success', 'Password has been changed successfully.');
+                return redirect()->route('client.account.index')->with('message', 'Password has been changed successfully.');
             } else {
                 return redirect()->route('client.account.changepassword')->with('error', 'Password and Confirm Password does not match.');
             }
@@ -44,24 +48,45 @@ class AccountManagement extends Controller
         $user = Auth::user();
         if ($request->isMethod('post')) {
             $data = $request->all();
-    
+
             $request->validate([
                 'name' => 'required|string|max:255',
                 'email' => 'required|string|email|max:255|unique:users,email,' . $user->id,
+                'prefecture' => 'nullable|string|max:255',
+                'city' => 'nullable|string|max:255',
                 'address' => 'nullable|string|max:255',
-                'phone' => 'nullable|string|max:10',
+                'building_name' => 'nullable|string|max:255',
+                'phone' => ['required', 'regex:/^\d{2}(?:-\d{4}-\d{4}|\d{8}|\d-\d{3,4}-\d{4})$/'],
             ]);
-    
+
+            $imageName = null;
+            if ($request->hasFile('avt')) {
+                if ($user->avt) {
+                    $oldImagePath = public_path('img/user'.$user->avt);
+                    if (file_exists($oldImagePath)) {
+                        unlink($oldImagePath);
+                    }
+                }
+                $imageName = str_replace(' ', '', time() . '_' . $request->avt->getClientOriginalName());
+                $request->avt->move(public_path('img/user/'), $imageName);
+            } else {
+                $imageName = $user->avt;
+            }
+
             $user->full_name = $data['name'];
             $user->email = $data['email'];
+            $user->prefecture = $data['prefecture'];
+            $user->city = $data['city'];
             $user->address = $data['address'];
+            $user->building_name = $data['building_name'];
             $user->phone = $data['phone'];
-            $user->save(); 
-    
-            return redirect()->route('client.account.index')->with('success', 'Update Info Success.');
+            $user->avt = $imageName;
+            $user->save();
+
+            return redirect()->route('client.account.index')->with('message', 'Your info has been updated.');
         }
-    
-        return view('client.Account.AccountManagement', compact('user'));   
+
+        return view('client.Account.AccountManagement', compact('user'));
     }
-    
+
 }
