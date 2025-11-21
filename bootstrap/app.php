466e95c9c342f61371ac0b8139717376ab95e0fb
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-11-21 14:57:02 +0700

    fix: update middleware to trust proxies and improve guest redirection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,18 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->redirectGuestsTo(fn (Request $request) => route('client.login.index'));
+        $middleware->trustProxies([
+            '10.0.0.0/8',
+            '172.16.0.0/12',
+            '192.168.0.0/16',
+        ]);
+
+        $middleware->redirectGuestsTo(fn(Request $request) => route('client.login.index'));
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
